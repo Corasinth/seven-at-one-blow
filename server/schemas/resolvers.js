8bd8d6cdcc760cca62e3cee5148c29727fe6d8b6
timcs <timcs1274@gmail.com>
2022-08-31 16:50:09 -0400

    add username query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,17 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if(context.player) {
+                const playerData = await Player.findOne({})
+                .select('-__v -password')
+                // .populate('')?
+                return playerData;
+            }
 
+            throw new AuthenticationError('Not logged in')
+
+        }
     },
 
     Mutation: {
