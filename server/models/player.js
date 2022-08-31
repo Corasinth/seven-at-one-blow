f65b1006fe6a992be06c9e2081d17a0eeb897517
timcs <timcs1274@gmail.com>
2022-08-31 13:58:26 -0400

    add player model

---
+++ server/models/player.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const userProfile = new Schema({
+username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+},
+password: {
+    type: String,
+    trim: true,
+},
+name: {
+    type: String,
+    trim: true,
+}
+});
+
+
+const player = model('Player', userProfile);
+
+module.export = player;
