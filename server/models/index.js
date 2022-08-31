fa072c31b8afe7947e7bdc9c5a89c3066838d016
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-31 12:41:25 -0500

    create index for models

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const Story = require('./Story');
+const Player = require('./player');
+const Item = require('./item');
+const Monster = require('./monster');
+const Npc = require('./npc');
+
+module.exports = { Story, Player, Item, Monster, Npc };
