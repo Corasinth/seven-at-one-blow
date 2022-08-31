52cfeb5842aa63a1f4587c1e2e2056d9f99bd767
timcs <timcs1274@gmail.com>
2022-08-31 15:04:40 -0400

    add bcrypt and password hash

--- server/models/player.js
+++ server/models/player.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userProfile = new Schema({
 default: {
@@ -21,6 +22,19 @@ name: {
 },
 playerSave: playerSaveSchema
 });
+
+
+// hash password
+userProfile.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next()
+});
+
+
+
 
 
 const player = model('Player', userProfile);
