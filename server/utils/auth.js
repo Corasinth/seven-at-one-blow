56c231b9a6e44b9c4f3913855308e56b646876bb
apark0819 <apark0819@gmail.com>
2022-08-31 12:16:20 -0700

    jwt auth.js file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,4 +3,10 @@ const jwt = require('jsonwebtoken');
 const secret = 'merlin'
 const expiration = '24h';
 
-module.exports = {}
+module.exports = module.exports = {
+    signToken: function ({ email, username, _id }) {
+      const payload = { email, username, _id };
+      return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+  };
+  
