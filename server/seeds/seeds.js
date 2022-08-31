ab029577d57bfe28ddddbb024ff78e7f29d694de
timcs <timcs1274@gmail.com>
2022-08-31 14:54:57 -0400

    add basic seed files

---
+++ server/seeds/seeds.js
@@ -0,0 +1,8 @@
+
+
+const { item, monsters, npc, story } = require('../models');
+
+const itemData = require('./item.json');
+const monsterData = require('./monsters.json');
+const npcData = require('./npc.json');
+const storyData = require('./story.json');
