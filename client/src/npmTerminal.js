8274c5568655163d3898a35d8dfc5c7dced0fbab
timcs <timcs1274@gmail.com>
2022-09-01 23:19:34 -0400

    add print text to terminal function

--- client/src/npmTerminal.js
+++ client/src/npmTerminal.js
@@ -22,14 +22,27 @@ class TermPackage extends Component {
           commands={{
             'console-log': () => {console.log('Success!')},
             showmsg: this.showMsg,
-            popup: () => alert('Terminal in React')
+            popup: () => alert('Terminal in React'),
+
+            // this prints text the text to the terminal
+            'type-text': (args, print, runCommand) => {
+                const text = args.slice(1).join(' ');
+                print('THIS TEXT GETS DISPLAYED');
+                for (let i = 0; i < text.length; i += 1) {
+                  setTimeout(() => {
+                    runCommand(`edit-line ${text.slice(0, i + 1)}`);
+                  }, 100 * i);
+                }
+              }
 
           }}
+
           descriptions={{
             'open-google': 'opens google.com',
             showmsg: 'shows a message',
             alert: 'alert', popup: 'alert'
           }}
+
           // message that appears when you start the terminal
           msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
         />
