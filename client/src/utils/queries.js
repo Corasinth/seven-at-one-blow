a69af404810ae1720bcc9a49c1c948f6c0dd2bff
apark0819 <apark0819@gmail.com>
2022-09-02 10:52:16 -0700

    loadsave query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,9 +1,14 @@
 import { gql } from '@apollo/client';
 
+
 export const SAVE = gql`
   query loadSave {
     loadSave {
-
+      _id
+      username
+      password
+      name
+      playerSave
     }
   }
 `;
