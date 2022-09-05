e9e00dc82b5f490bd0bebc9ac03237e84866ab5c
apark0819 <apark0819@gmail.com>
2022-09-05 16:14:07 -0700

    login function

--- client/src/utils/userCommands/loginCmd.js
+++ client/src/utils/userCommands/loginCmd.js
@@ -14,3 +14,15 @@
         password: "Park"
     }
  ]
+ function Login (){
+    var username = "";
+    var password = "";
+
+    for (var i = 0; i < testAccount.length; i++){
+        if (username === testAccount[i].username && password === testAccount[i].password){
+            return "Thanks" + username + "for logging in!"
+        } else {
+            return "Please enter the correct login information to proceed!"
+        }
+    }
+ }
