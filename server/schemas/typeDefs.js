8e21505a93821881b0e07593f1fa9b2aff2af69c
Corasinth <beforeyouknowkindness@gmail.com>
2022-09-05 16:40:49 -0500

    create typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,22 +4,53 @@ const typeDefs = gql`
     type Player {
         _id: ID!
         username: String
-        name: String
         password: String
-        playerSave: ID 
+        playerSave: [Number] 
+        inventory: [String]
+    }
+
+    type Stage {
+        chapterNumber: Number
+        numberOfStages: Number
+    }
+
+    type Story {
+        _id: ID!
+        discoveringGreateness: newWord
+        giantTrials: newWord
+        royalTrifle: newWord
+        NarrowEscape: newWord
+        RegalResolve: newWord
+        textMatrix: [Array]
+    }
+
+    type Item {
+        _id: ID!
+        name: String!
+        relevantStages: [Array]
+        scriptCoordinates: [Array]
+        requiredInInventoryStages: [Array]
+        inventoryScriptCoordinates: [Array]
     }
 
     type Auth {
         token: ID! 
-        user: Player
+        player: Player
+    }
+
+    type StoryData {
+        story: Story
+        items: Item
     }
 
     type Query {
-        loadSave: Player
+        getStoryData: StoryData
     }
 
     type Mutation {
         login(username:String!, password:String!): Auth
+        savePlayer(username:String!, storySave: [Number]!, inventory:[String]!)
+        newPlayer(username:String!, password:String!): Auth
     }
 `
 module.exports = typeDefs;
