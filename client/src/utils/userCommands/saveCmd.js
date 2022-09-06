53bebccb484a75f8512edb02434a04dc3d9f2c76
apark0819 <apark0819@gmail.com>
2022-09-06 15:28:55 -0700

    save player cmd

--- client/src/utils/userCommands/saveCmd.js
+++ client/src/utils/userCommands/saveCmd.js
@@ -1,2 +1,11 @@
  
+ import { SAVE_PLAYER } from '../mutations.js'
+ import { useMutation } from '@apollo/client'
+ import Auth from '../auth.js'
+
+ function savePlayer(){
+
+ }
+
+ export default savePlayer;
  
