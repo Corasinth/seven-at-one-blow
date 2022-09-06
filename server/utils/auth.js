0b7fb144402d1dbea0dffe79114bcab7844d6cdd
Corasinth <beforeyouknowkindness@gmail.com>
2022-09-05 23:36:07 -0500

    add boilerplate jwt auth files

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,12 +1,31 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'merlin'
+const secret = 'luckyNeedle'
 const expiration = '24h';
 
 module.exports = module.exports = {
-    signToken: function ({ username, _id }) {
-      const payload = { username, _id };
-      return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
-  };
-  
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
+  signToken: function ({ username, _id }) {
+    const payload = { username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
