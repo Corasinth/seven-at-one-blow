6749915e771cffe29e5d39c7165684efcee8aa98
apark0819 <apark0819@gmail.com>
2022-09-05 20:29:40 -0700

    base login cmd

--- client/src/utils/userCommands/loginCmd.js
+++ client/src/utils/userCommands/loginCmd.js
@@ -1,28 +1,28 @@
- var testAccount = [
-    {
-        username: "Tim",
-        password: "Sasse"
-    },
+//  var testAccount = [
+//     {
+//         username: "Tim",
+//         password: "Sasse"
+//     },
 
-    {
-        username: "Abdur-Rauf",
-        password: "Ahmed"
-    },
+//     {
+//         username: "Abdur-Rauf",
+//         password: "Ahmed"
+//     },
 
-    {
-        username: "Austin",
-        password: "Park"
-    }
- ]
- function Login (){
-    var username = "";
-    var password = "";
+//     {
+//         username: "Austin",
+//         password: "Park"
+//     }
+//  ]
+//  function Login (){
+//     var username = "";
+//     var password = "";
 
-    for (var i = 0; i < testAccount.length; i++){
-        if (username.input === testAccount[i].username && password.input === testAccount[i].password){
-            return "Thanks" + username + "for logging in!"
-        } else {
-            return "Please enter the correct login information to proceed!"
-        }
-    }
- }
+//     for (var i = 0; i < testAccount.length; i++){
+//         if (username.input === testAccount[i].username && password.input === testAccount[i].password){
+//             return "Thanks" + username + "for logging in!"
+//         } else {
+//             return "Please enter the correct login information to proceed!"
+//         }
+//     }
+//  }
