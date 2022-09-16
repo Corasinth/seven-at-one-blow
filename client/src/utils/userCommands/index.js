30c22560e0f7c5a385c5f11c99f8ef73f232e8ae
Corasinth <beforeyouknowkindness@gmail.com>
2022-09-16 16:09:30 -0500

    add index for commands

---
+++ client/src/utils/userCommands/index.js
@@ -0,0 +1,7 @@
+import newPlayer from './signup';
+import login from './login';
+import newGame from './newgame';
+import itemInteraction from './itemInteraction';
+import displayChapter from './display-chapter';
+
+export default {signup, login, newGame, itemInteraction, displayChapter}
