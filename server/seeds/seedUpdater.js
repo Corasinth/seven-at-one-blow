01e9f5204ea699fc3a70df0713e119f2be1c64db
Corasinth <beforeyouknowkindness@gmail.com>
2022-09-17 18:08:20 -0500

    update seedupdater to update chapter stages

--- server/seeds/seedUpdater.js
+++ server/seeds/seedUpdater.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path')
-const filePath = path.join(__dirname, './item.json');
+const itemFilePath = path.join(__dirname, './item.json');
+const storyFilePath = path.join(__dirname, './story.json');
 
 //If you want to run this editor as a script (and have the resulting json file auto format) paste this into your console with the appropriate arguments filled out: 
 //'npm run-script itemEditor --chapt=<your chapter number> --pi=<your insertion/deletion point> --iod=<increment of decrement, depending on whether you added or deleted text>
@@ -14,25 +15,27 @@ const addedOrDeleted = process.env.npm_config_iod;
  * @param {The index of the entry that you've deleted, or at which you've inserted} pivotStage 
  * @param {A string that can be 'increment' or 'decrement' depending on whether you've added or deleted text} incrementOrDecrement 
  */
-async function itemSeedEditor (chapter, pivotStage, incrementOrDecrement) {
-    const itemJSONData = fs.readFileSync (filePath);
+async function itemSeedEditor(chapter, pivotStage, incrementOrDecrement) {
+    const itemJSONData = fs.readFileSync(itemFilePath);
     const items = JSON.parse(itemJSONData);
+    const storyJSONData = fs.readFileSync(storyFilePath);
+    const story = JSON.parse(storyJSONData)
     const editor = (num) => {
-            if (num >= pivotStage) {
-                if (incrementOrDecrement === 'increment') {
-                    return ++num;
-                } else {
-                    return --num;
-                }
+        if (num >= pivotStage) {
+            if (incrementOrDecrement === 'increment') {
+                return ++num;
+            } else {
+                return --num;
             }
-            return num
         }
+        return num
+    }
     for (let item of items) {
         for (let key in item) {
             if (key === 'name') {
                 continue;
             }
-            
+
             for (let coordinate of item[key]) {
                 if (parseInt(coordinate[0]) === chapter) {
                     coordinate[1] = editor(coordinate[1]).toString();
@@ -40,14 +43,26 @@ async function itemSeedEditor (chapter, pivotStage, incrementOrDecrement) {
             }
         }
     }
-    fs.writeFileSync(filePath, JSON.stringify(items))
-    console.log('Item seeds edited')
+    //Calling chapters story sections here so as not to be confused with the parameter value
+    //This for loop is required to update the story's stage counts, essential to letting the game know when it's time for a new chapter
+    for (let storySection of story.chapters) {
+        if (storySection.chapterNumber === chapter && pivotStage < storySection.numberOfStages) {
+            if (incrementOrDecrement === 'increment') {
+                storySection.numberOfStages += 1;
+            } else {
+                storySection.numberOfStages -= 1;
+            }
+        }
+        fs.writeFileSync(itemFilePath, JSON.stringify(items))
+        fs.writeFileSync(storyFilePath, JSON.stringify(story))
+        console.log('Item seeds edited')
+    }
 }
 
-(function main(){
-    if(editedChapter && indexOfEditedText && addedOrDeleted) {
-        itemSeedEditor(editedChapter, indexOfEditedText, addedOrDeleted);
-    } else {
-        console.log('Please ensure you pass all nessecary arguments when running this file')
-    }
-})()
+    (function main() {
+        if (editedChapter && indexOfEditedText && addedOrDeleted) {
+            itemSeedEditor(editedChapter, indexOfEditedText, addedOrDeleted);
+        } else {
+            console.log('Please ensure you pass all nessecary arguments when running this file')
+        }
+    })()
